ff8e7c06665fbc69bbe969a995d420e9b6a3a583
Abhishek S <s.abhishek8@tcs.com>
2021-12-22 13:23:14 +0530

    Add get method to shorten url

--- api.py
+++ api.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from utils import shorten_url
 
 app = FastAPI()
 
 @app.get("/ping")
 def root():
     return {"message": "ok"}
+
+@app.get('/shorten')
+def get_original_link(url: str):
+    """
+    Accepts url and returns it's shortened version
+    """
+    short_url = shorten_url(url)
+    return short_url
